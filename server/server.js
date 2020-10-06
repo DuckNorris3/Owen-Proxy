3544cdace4a773a2ad1d029540877069d616c50f
Owen Wexler <owenbwexler@gmail.com>
2020-10-06 15:03:05 -0400

    Display 3rd service on proxy

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
   console.log('Tenthop proxy server listening on port ', PORT);
 });
 
 app.use(express.static('./client'));
+
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root: './client/' });
+});
